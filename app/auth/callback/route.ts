acd01e4ab3c277d57d3dc9c29dc7c05af05055f4
anubus298 <araristaf@gmail.com>
2024-02-25 21:32:53 +0100

    pushing to debug2

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,56 +6,50 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/utils/supabase/supabase";
 
 export async function GET(request: Request) {
-  try {
-    const requestUrl = new URL(request.url);
-    const code = requestUrl.searchParams.get("code");
-    const isReset: string | null = requestUrl.searchParams.get(
-      "isReset"
-    ) as string;
-    const cookieStore = cookies();
-    const supabase = createRouteHandlerClient<Database>({
-      cookies: () => cookieStore,
-    });
-    if (code) {
-      await supabase.auth.exchangeCodeForSession(code);
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+  const isReset: string | null = requestUrl.searchParams.get(
+    "isReset"
+  ) as string;
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient<Database>({
+    cookies: () => cookieStore,
+  });
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!isReset) {
+    const { data: profile, error: profile_error } = await supabase
+      .from("profiles")
+      .select("is_first_initialised")
+      .eq("id", user?.id as string)
+      .limit(1);
+    const { data: personal_info, error: personal_info_error } = await supabase
+      .from("personal_info")
+      .select("is_first_initialised")
+      .eq("id", user?.id as string)
+      .limit(1);
+    if (personal_info_error) {
+      return redirect(
+        "/auth/signIn?error=1&message=Could not authenticate user : " +
+          personal_info_error.message +
+          ", details :" +
+          personal_info_error.details
+      );
     }
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!isReset) {
-      const { data: profile, error: profile_error } = await supabase
-        .from("profiles")
-        .select("is_first_initialised")
-        .eq("id", user?.id as string)
-        .limit(1);
-      const { data: personal_info, error: personal_info_error } = await supabase
-        .from("personal_info")
-        .select("is_first_initialised")
-        .eq("id", user?.id as string)
-        .limit(1);
-      if (personal_info_error) {
-        return redirect(
-          "/auth/signIn?error=1&message=Could not authenticate user : " +
-            personal_info_error.message +
-            ", details :" +
-            personal_info_error.details
-        );
-      }
-      if (!profile?.[0].is_first_initialised) {
-        return redirect("/constructors/newAccount");
-      } else if (!personal_info?.[0].is_first_initialised) {
-        return redirect("/constructors/finishAccount");
-      }
-      if (profile) {
-        return redirect("/?islogged");
-      }
-      return NextResponse.redirect("/");
-    } else {
-      return NextResponse.redirect("/auth/passwordReset/update");
+    if (!profile?.[0].is_first_initialised) {
+      return redirect("/constructors/newAccount");
+    } else if (!personal_info?.[0].is_first_initialised) {
+      return redirect("/constructors/finishAccount");
     }
-  } catch (error: any) {
-    return redirect(
-      "/auth/signIn?error=1&message=Error catched :" + error.message
-    );
+    if (profile) {
+      return redirect("/?islogged");
+    }
+    return NextResponse.redirect("/");
+  } else {
+    return NextResponse.redirect("/auth/passwordReset/update");
   }
 }
